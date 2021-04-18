bc3ad221bfacc742bbae3889b55da9380da4b2ec
mohamedragaey <mohamedragaey33@gmail.com>
2021-04-18 15:38:44 +0200

    Applaying a fix to the dropdown delay issue on mobile and ios.

--- src/js/partials/custom.js
+++ src/js/partials/custom.js
@@ -1,12 +1,11 @@
 /* global $ */
 
 $(document).ready(function () {
-  $('.custom-select').on('click', function (e) {
+  $('.custom-select').on('click keyup keypress blur change', function (e) {
     e.stopPropagation()
     e.preventDefault()
     let content = $(this).parent('.block-body').find('.custom-select-content')
     content.each(function () {
-      $(this).blur()
       if (e.target.value === $(this).attr('data-dropdown')) {
         $(this).removeClass('hidden')
       } else {
